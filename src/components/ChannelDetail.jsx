15694a3af8a38d623327187f7636dfb5f60e0a34
opurbo007 <opurboujjol@gmail.com>
2024-02-18 02:59:08 +0600

    18-2-2024

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -1,7 +1,46 @@
-import React from "react";
+import { Box } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { fetchAPI } from "../utils/fetchAPI";
+import { ChannelCard, Videos } from "./index";
 
 const ChannelDetail = () => {
-   return <div>ChannelDetail</div>;
+   const [channelDetail, setChannelDetail] = useState();
+   const [video, setVideo] = useState(null);
+   const { id } = useParams();
+
+   useEffect(() => {
+      const fetchResults = async () => {
+         const data = await fetchAPI(`channels?part=snippet&id=${id}`);
+         setChannelDetail(data?.items[0]);
+
+         const videodata = await fetchAPI(
+            `search?channelId=${id}&part=snippet&order=date`
+         );
+         setVideo(videodata?.items);
+      };
+      fetchResults();
+   }, [id]);
+
+   return (
+      <Box minHeight="95vh">
+         <Box>
+            <div
+               style={{
+                  height: "300px",
+                  background:
+                     "linear-gradient(90deg, rgba(0,238,247,1) 0%, rgba(206,3,184,1) 100%, rgba(0,212,255,1) 100%)",
+                  zIndex: 10,
+               }}
+            />
+            <ChannelCard channelDetail={channelDetail} marginTop="-93px" />
+         </Box>
+         <Box p={2} display="flex">
+            <Box sx={{ mr: { sm: "100px" } }} />
+            <Videos videos={video} />
+         </Box>
+      </Box>
+   );
 };
 
 export default ChannelDetail;
--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,9 +1,23 @@
+import { Box, Stack } from "@mui/material";
 import React from "react";
+import { ChannelCard, VideoCard } from "./index";
 
 const Videos = ({ videos }) => {
-   console.log(videos);
-
-   return <div>consol</div>;
+   return (
+      <Stack
+         direction={"row"}
+         flexWrap={"wrap"}
+         justifyContent={"start"}
+         gap={2}
+      >
+         {videos.map((item, idx) => (
+            <Box key={idx}>
+               {item.id.videoId && <VideoCard video={item} />}
+               {item.id.channelId && <ChannelCard channelDetail={item} />}
+            </Box>
+         ))}
+      </Stack>
+   );
 };
 
 export default Videos;
